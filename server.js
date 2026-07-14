a156deae07adbc6a02c3cb073c5bd731cc38406a
outragedfinn <finnwatkinson@protonmail.com>
2026-07-14 06:59:24 +0000

    Remove node_modules from git

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ app.get("/", (req,res)=>{
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server listening on ${PORT}`);
+app.listen(PORT, '127.0.0.1', () => {
+    console.log(`Server is running`);
 });
